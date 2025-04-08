9780ad8c46615a6b728a121c0064a36de8b06c46
Tycho Tatitscheff <tychot@bam.tech>
2025-04-08 19:10:32 +0200

    chore: configure app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+// Mantine
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
