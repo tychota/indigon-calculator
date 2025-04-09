2f202b1730c52187358fc7cc59ce851334895920
Tycho Tatitscheff <tychot@bam.tech>
2025-04-09 02:11:23 +0200

    chore: improve build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // Check for mantine packages first
+            if (id.includes("@mantine") || id.includes("mantine-react-table")) {
+              return "mantine-vendor";
+            }
+            // Next, group React libraries
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "react-vendor";
+            }
+            // Group recharts separately
+            if (id.includes("recharts")) {
+              return "recharts-vendor";
+            }
+            // Everything else goes into vendor.
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
+});
